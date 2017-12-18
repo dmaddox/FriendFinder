ffc72d71d8f72d6484c94be5b103d05e6b81233a
dmaddox <dmaddox@gmail.com>
2017-12-18 16:41:27 -0600

    working html routing in separate files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,25 +1,23 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
 
-var app = express();
 
-
-// A GET Route to /survey which should display the survey page.
-app.get('/survey', function(req, res) {
-	res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
-
-// A default, catch-all route that leads to home.html which displays the home page.
-app.get('/:name?', function(req, res) {
-	if (req.params.name !== 'survey') {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
-	};
-});
-
-// Listening from port 8080
-app.listen(8080, function() {
-  console.log("App listening on PORT 8080");
-});
+//mult routes
+module.exports = {
+	survey: function(app) {
+		// A GET Route to /survey which should display the survey page.
+		app.get('/survey', function(req, res) {
+			res.sendFile(path.join(__dirname, "../public/survey.html"));
+		});
+	},
+	home: function(app) {
+		// A default, catch-all route that leads to home.html which displays the home page.
+		app.get('/:name?', function(req, res) {
+			if (req.params.name !== 'survey') {
+				res.sendFile(path.join(__dirname, "../public/home.html"));
+			};
+		});
+	}
+};
--- server.js
+++ server.js
@@ -3,3 +3,32 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3003;
+
+
+
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+// Routing
+	// HTML
+	htmlRoutes.survey(app);
+	htmlRoutes.home(app);
+
+
+
+///study THIS IS RECORDED in study hall!
+
+
+// apiRoutes(app)
+
+
+// //mult routes
+// apiRoutes.apiRoutes1(app)

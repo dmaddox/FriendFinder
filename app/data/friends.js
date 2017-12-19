de142f00636711257205f8aa7108876b2c65ac04
dmaddox <dmaddox@gmail.com>
2017-12-19 17:06:14 -0600

    updated friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,104 @@
 module.exports = [
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Kim Kardashian",
+    "photo":"https://media.npr.org/assets/img/2015/06/17/Kim%20Kardashian-8b12194250ba930485f40c0d56b0176b5f26440d-s900-c85.jpg",
+    "info": "She gives attention, but not as much as she needs attention. You've matched with Kim K!",
     "scores":[
         1,
         1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
+        5,
+        3,
+        2,
+        3,
+        5,
+        3,
+        3,
+        5
       ]
-  }
+  },
+  {
+    "name":"Kanye West",
+    "photo":"https://sslg.ulximg.com/image/405x405/artist/1392767353_217f16228baaa5dc18c82925ac76edf6.jpg/9e89e114db44f266e044addd06e88d69/1392767353_kanye_west_wall_40.jpg",
+    "info": "A boy after both everyone's and no one's hearts... his own lord and savior, Yeezus.",
+    "scores":[
+        3,
+        4,
+        1,
+        1,
+        1,
+        4,
+        3,
+        5,
+        1,
+        3
+      ]
+  },
+  {
+    "name":"Taylor Swift",
+    "photo":"https://stephaniemarksbooks.com/wp-content/uploads/2017/09/TaylorSwift.jpg",
+    "info": "I hope you like pop, because this girl will never stop... really, never...",
+    "scores":[
+        5,
+        5,
+        4,
+        1,
+        2,
+        1,
+        1,
+        3,
+        1,
+        3
+      ]
+  },
+  {
+    "name":"Chris & Luke Hemsworth",
+    "photo":"https://media1.popsugar-assets.com/files/thumbor/iFD-RmvIPEZx7grv3ofTFRVSMd8/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/08/10/812/n/1922398/ff5860b3c84787aa_GettyImages-113557962/i/Hemsworth-brothers-looked-dapper-LA-afterparty-Thor.jpg",
+    "info": "Who know's which is which, but who needs to. They are probably better together!",
+    "scores":[
+        4,
+        2,
+        5,
+        1,
+        5,
+        1,
+        3,
+        3,
+        3,
+        5
+      ]
+  },
+  {
+    "name":"Donald Trump",
+    "photo":"https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/05/12/104466932-PE_Color.240x240.jpg?v=1494613853",
+    "info": "Yikes.",
+    "scores":[
+        5,
+        3,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5
+      ]
+  },
+    {
+      "name":"Amy Poehler",
+      "photo":"https://www.gannett-cdn.com/-mm-/0c9109c71ea0524d9fe840f91fabd67bb94a26a9/r=537&c=0-0-534-712/local/-/media/USATODAY/USATODAY/2013/01/29/gty-160215139-3_4.jpg",
+      "info": "She'll keep you laughing and keep you thoughtful!",
+      "scores":[
+          4,
+          1,
+          4,
+          1,
+          4,
+          1,
+          3,
+          1,
+          2,
+          3
+        ]
+    }
 ];
